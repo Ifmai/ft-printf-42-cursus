04a453bf42ff0a13a3e4c93b9f0660944af5404c
Ifmai <94466351+Ifmai@users.noreply.github.com>
2022-10-18 09:52:42 +0300

    norminette fix

--- src/ft_printf.h
+++ src/ft_printf.h
@@ -1,16 +1,28 @@
-#ifndef FT_PRINTF_H
-#define FT_PRINTF_H
-#include <unistd.h>
-#include <stdarg.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hozdemir <hozdemir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 09:46:41 by hozdemir          #+#    #+#             */
+/*   Updated: 2022/10/18 09:48:55 by hozdemir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "../libft/libft.h"
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <unistd.h>
+# include <stdarg.h>
+
+# include "../libft/libft.h"
 
 typedef struct t_listt
 {
-	char 			flagstart;
-	long 			dec;
-	int 			cvalue;
-	char 			*string;
+	char			flagstart;
+	long			dec;
+	int				cvalue;
+	char			*string;
 	unsigned int	unsval;
 	unsigned int	xval;
 	unsigned long	pval;
@@ -18,15 +30,14 @@ typedef struct t_listt
 
 }					t_printf;
 
-void	check_arg(const char *str, va_list pars,t_printf *prnt);
-void	check_one(va_list,t_printf *prnt);
+void	check_arg(const char *str, va_list pars, t_printf *prnt);
+void	check_one(va_list pars, t_printf *prnt);
 void	check_two(va_list pars, t_printf *prnt);
 void	check_tree(va_list pars, t_printf *prnt);
 void	decimal_to_hexa(t_printf *prnt, unsigned int nb);
 void	decimal_to_hexap(t_printf *prnt, unsigned long nb);
 void	unsigned_convert(t_printf *print, unsigned int nb);
 void	reset_struck(t_printf *p);
-int		ft_printf(const char *, ...);
-
+int		ft_printf(const char *str, ...);
 
 #endif
