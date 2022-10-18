5c99c131a46cd67599b7d808f171cb71d225c312
Ifmai <94466351+Ifmai@users.noreply.github.com>
2022-10-18 14:25:21 +0300

    update main function

--- srcb/ft_printf_bonus.c
+++ srcb/ft_printf_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hozdemir <hozdemir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 08:12:36 by hozdemir          #+#    #+#             */
-/*   Updated: 2022/10/18 08:13:34 by hozdemir         ###   ########.fr       */
+/*   Updated: 2022/10/18 11:03:00 by hozdemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 int	ft_printf(const char *str, ...)
 {
 	va_list		arg;
-	t_printfb	specter;
+	t_printfb	print;
 
 	va_start(arg, str);
-	specter.rtn = 0;
-	ft_reset(&specter);
-	ft_check_arg(str, arg, &specter);
+	print.rtn = 0;
+	ft_reset(&print);
+	ft_check_arg(str, arg, &print);
 	va_end(arg);
-	return (specter.rtn);
+	return (print.rtn);
 }
